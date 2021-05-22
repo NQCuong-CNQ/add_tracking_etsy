3b2c88ac85296cb455f0457df00110db49423684
cuong <nqcuong9a5@gmail.com>
2021-05-22 04:01:40 -0700

    done

--- etsy_server.js
+++ etsy_server.js
@@ -1,30 +1,42 @@
-// var express = require("express")
-// var app = express()
-// const http = require("http")
-
-// var server = http.createServer(app)
-
 var io = require('socket.io-client')
 var socket = io.connect("https://giftsvk.com", {
   port: 443,
   reconnect: true
 })
 
-// server.listen(5555)
-
 const sendkeys = require('sendkeys-js/index.js')
 
-main()
-async function main() {
-  sendkeys.send('%{ESC}')
+socket.on("track-order-step2", async function (data) {
+  console.log('step 1')
+  if(data['name'] == 'lynLL'){
+    console.log('step 2')
+    sendkeys.send('^{f}')
+    await sleep(1000)
+    sendkeys.send('{i}{t}{e}{m}')
+    await sleep(1000)
+    sendkeys.send('{ESC}')
+    await sleep(1000)
+    sendkeys.send('{TAB}')
+    await sleep(1000)
+    sendkeys.send('{TAB}')
 
-  // socket.on("test", async function (data) {
-  //   console.log(data)
-  // })
-}
+    console.log(data)
+
+    let number = data['number_tracking'].split('')
+    for (let i = 0; i < number.length; i++) {
+      sendkeys.send(`{${number[i]}}`)
+      await sleep(50)
+    }
+
+    await sleep(2000)
+    sendkeys.send('{ESC}')
+    socket.emit('track-order-step3', data['name'])
+    console.log('step 3')
+  }
+})
 
 async function sleep(ms) {
   return new Promise(
     resolve => setTimeout(resolve, ms)
-  );
+  )
 }
