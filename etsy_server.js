3a381dcc0fe490c4f8e68d57b8067edb12b448c9
cuong <nqcuong9a5@gmail.com>
2021-05-22 11:13:17 +0700

    dfs

--- etsy_server.js
+++ etsy_server.js
@@ -1,41 +1,22 @@
 // var express = require("express")
 // var app = express()
 // const http = require("http")
-// const puppeteer = require('puppeteer-extra')
-// const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-// puppeteer.use(StealthPlugin())
-// const PuppUtils = require('./lib/PuppUtils')
 
-// var ks = require('node-key-sender')
-
-// const { exec } = require('child_process')
-const { keyboard, Key, mouse, left, right, up, down, screen } = require("@nut-tree/nut-js")
-// ks.sendCombination(['control', 'a']);
 // var server = http.createServer(app)
 
-// var io = require('socket.io-client')
-// var socket = io.connect("https://giftsvk.com", {
-//   port: 443,
-//   reconnect: true
-// })
+var io = require('socket.io-client')
+var socket = io.connect("https://giftsvk.com", {
+  port: 443,
+  reconnect: true
+})
 
 // server.listen(5555)
 
+const sendkeys = require('sendkeys-js/index.js')
 
 main()
 async function main() {
-  await keyboard.pressKey(Key.RightAlt);
-  await keyboard.pressKey(Key.Tab);
-  await keyboard.releaseKey(Key.Tab);
-  await keyboard.releaseKey(Key.RightAlt);
-
-  // exec('java -jar "C:/Users/pc/Desktop/add_tracking_etsy/node_modules/node-key-sender/jar/key-sender.jar" alt-tab')
-  // await sleep(1000)
-  // exec('java -jar "C:/Users/pc/Desktop/add_tracking_etsy/node_modules/node-key-sender/jar/key-sender.jar" tab')
-  // await sleep(1000)
-  // exec('java -jar "C:/Users/pc/Desktop/add_tracking_etsy/node_modules/node-key-sender/jar/key-sender.jar" a-d-c-g-e')
-  // await sleep(1000)
-  // exec('java -jar "C:/Users/pc/Desktop/add_tracking_etsy/node_modules/node-key-sender/jar/key-sender.jar" enter')
+  sendkeys.send('%{ESC}')
 
   // socket.on("test", async function (data) {
   //   console.log(data)
